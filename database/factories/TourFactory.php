28934789e88d948ab623722c80a3d031a08f9aee
Predrag Jovanović <160655253+Pedja3@users.noreply.github.com>
2025-11-24 16:30:54 +0100

    Update TourFactory.php

--- database/factories/TourFactory.php
+++ database/factories/TourFactory.php
@@ -5,17 +5,11 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Tour;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tour>
- */
+
 class TourFactory extends Factory
 {
     protected $model = Tour::class;
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+   
     public function definition(): array
     {
         $difficulties = ['easy', 'medium', 'hard'];
