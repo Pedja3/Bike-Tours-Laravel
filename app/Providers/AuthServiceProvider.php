5cc5b53a650dc9f46d09dc34ea71ac523f0ed751
Predrag Jovanović <160655253+Pedja3@users.noreply.github.com>
2025-12-04 20:12:31 +0100

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,18 +8,10 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
         Tour::class => TourPolicy::class,
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         $this->registerPolicies();
